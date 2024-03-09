1498f9997f706c52384511b3ec261f17aaf64f5d
GiveUpNaming <dino040806@gmail.com>
2024-03-09 22:07:45 +0800

    Check if vector is NULL.

--- vector.c
+++ vector.c
@@ -16,21 +16,30 @@ Vector *vector_create(void) {
 }
 
 void vector_push(Vector *vector, double element) {
+  if (vector==NULL){
+    return;
+  }
   if (vector->size == vector->capacity) {
     vector->capacity *= 2;
     vector->data = realloc(vector->data, sizeof(double) * vector->capacity);
     if(vector->data==NULL){
-      return NULL;
+      return;
     }
   }
   vector->data[vector->size++] = element;
 }
 
 double vector_get(const Vector *vector, int index) {
+  if (vector==NULL){
+    return 0;
+  }
   return vector->data[index];
 }
 
 void vector_free(Vector *vector) {
+  if (vector==NULL){
+    return;
+  }
   free(vector->data);
   free(vector);
 }
